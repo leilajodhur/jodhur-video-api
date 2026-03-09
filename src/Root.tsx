718ab9f379e4ed74173e12de36d10af715f2be73
leilajodhur <leila.jodhur@gmail.com>
2026-03-09 14:52:33 +0000

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Composition } from 'remotion';
+// استيراد أدوات تحميل الخطوط
+import { loadFont as loadCairo } from "@remotion/google-fonts/Cairo";
+import { loadFont as loadPlayfair } from "@remotion/google-fonts/PlayfairDisplay";
+import { loadFont as loadInter } from "@remotion/google-fonts/Inter";
 
 import T1_PromoFlash from './templates/T1_PromoFlash';
 import T2_HeritageStory from './templates/T2_HeritageStory';
 import T3_ProduitRegion from './templates/T3_ProduitRegion';
 import T4_TutorialRoutine from './templates/T4_TutorialRoutine';
 import T5_CarouselAnime from './templates/T5_CarouselAnime';
+
+// إخبار Remotion بانتظار تحميل الخطوط قبل الرندر
+const waitForFont = delayRender();
+Promise.all([
+  loadCairo(), 
+  loadPlayfair(), 
+  loadInter()
+]).then(() => {
+  continueRender(waitForFont);
+}).catch((err) => {
+  console.error("خطأ في تحميل الخطوط:", err);
+  continueRender(waitForFont); // الاستمرار حتى لو فشل التحميل لتجنب تعليق السيرفر
+});
 
 export const RemotionRoot: React.FC = () => {
   return (
