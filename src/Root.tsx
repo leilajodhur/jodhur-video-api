6365c82bfdd453c8a4abd1d2c79b1dacaab993e2
leilajodhur <leila.jodhur@gmail.com>
2026-03-09 09:13:12 +0000

    Update comments for duration and frames

--- src/Root.tsx
+++ src/Root.tsx
@@ -10,11 +10,11 @@ import T5_CarouselAnime from './templates/T5_CarouselAnime';
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      {/* القوالب العادية: 15 ثانية (450 إطار) */}
+      {/* 15 ثانية = 450 إطار */}
       <Composition id="T1-PromoFlash" component={T1_PromoFlash} durationInFrames={450} fps={30} width={1080} height={1920} />
       <Composition id="T2-HeritageStory" component={T2_HeritageStory} durationInFrames={450} fps={30} width={1080} height={1920} />
 
-      {/* القوالب المعقدة: 20 ثانية (600 إطار) */}
+      {/* 20 ثانية = 600 إطار */}
       <Composition id="T3-ProduitRegion" component={T3_ProduitRegion} durationInFrames={600} fps={30} width={1080} height={1920} />
       <Composition id="T4-TutorialRoutine" component={T4_TutorialRoutine} durationInFrames={600} fps={30} width={1080} height={1920} />
       <Composition id="T5-CarouselAnime" component={T5_CarouselAnime} durationInFrames={600} fps={30} width={1080} height={1920} />
